dfdb105e1fd4f8c0d71a92f8c8f26d8814f85e59
haidoe <haidren.amalia@gmail.com>
2023-05-06 13:48:18 -0700

    Post Router - Make sure username is not null

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,6 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -23,9 +24,22 @@ export const postRouter = createTRPCRouter({
 
     const filteredUsers = users.map(filterUserForClient);
 
-    return posts.map((post) => ({
-      post,
-      author: filteredUsers.find((user) => user.id === post.authorId),
-    }));
+    return posts.map((post) => {
+      const author = filteredUsers.find((user) => user.id === post.authorId);
+
+      if (!author || !author.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for Post not found.",
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
