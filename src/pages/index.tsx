849417388790b7b6039f6a74c6a13758c1828e98
haidoe <haidren.amalia@gmail.com>
2023-05-07 12:34:55 -0700

    Implemented NextImage with ClerkDevAssets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -18,10 +21,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-2">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="User's Profile Image"
-        className=" h-14 w-14 rounded-full"
+        className=" rounded-full"
+        width={56}
+        height={56}
       />
 
       <input
@@ -39,10 +44,12 @@ const PostView = (props: PostWithUser) => {
 
   return (
     <div key={post.id} className=" flex gap-2 border-b border-slate-400 p-4">
-      <img
+      <Image
         src={author.profileImageUrl}
         alt="Image of the Author"
-        className="h-14 w-14 rounded-full"
+        className="rounded-full"
+        width={56}
+        height={56}
       />
 
       <div className="flex flex-col">
