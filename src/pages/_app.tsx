923aead7cdf68527b302fa8488f44e3ef7a61132
haidoe <haidren.amalia@gmail.com>
2023-05-10 14:20:32 -0700

    Moved the Head, Metas to Global Scope

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 
@@ -8,6 +9,12 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
